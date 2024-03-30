6506806f57b6e34bf0b3ccebbcd6c48a91e647e8
Tolgahan Çelik <tol_celik@hotmail.com>
2024-03-30 18:34:32 +0300

    Installing any packages that may be required.

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    'react-native-reanimated/plugin',
+    [
+      '@babel/plugin-proposal-decorators',
+      {
+        legacy: true,
+      },
+    ],
+    [
+      'module-resolver',
+      {
+        alias: {
+          'react-native-sqlite-storage': 'react-native-quick-sqlite',
+        },
+      },
+    ],
+  ],
 };
